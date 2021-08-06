3d7978e6fb8be733d2159b315778224e994059d6
Arjun_Lubana <arjunlubana562@gmail.com>
2021-08-06 09:13:14 +0300

    Fixed: Clear questions and answers from the DOM after retry event

--- index.js
+++ index.js
@@ -6,23 +6,20 @@ var numberOfQuestions = 10;
 var score = 0;
 
 retry.addEventListener("click", () => {
-  while (answersContainer.firstChild) {
-    answersContainer.removeChild(answersContainer.lastChild);
-  }
-  questionElement.innerHTML = "";
   score = 0;
   main();
 });
+
 exit.addEventListener("click", () => {
   questionElement.innerHTML = "Thank you for playing!";
   while (answersContainer.firstChild) {
     answersContainer.removeChild(answersContainer.lastChild);
   }
-
 });
 //  https://opentdb.com/api.php?amount=10
 const getTrivia = async () => {
   const response = await fetch("https://opentdb.com/api.php?amount=10");
+  // const response = await fetch("data.json");
   const data = await response.json();
   return data.results;
 };
@@ -39,14 +36,6 @@ const setQuestion = (question) => {
   questionElement.innerHTML = question;
 };
 
-/**
- *
- * @param {*} options
- * @param {*} status
- * @param {*} generateQuestion
- * @description Renders the choices for the question to the DOM.
- * Binds an event listener to every answer onclick. The listener marks the current question and loads new question
- */
 const setAnswers = (options, status, generateQuestion) => {
   let correctAnswer = options[options.length - 1];
   for (answer of options) {
@@ -60,6 +49,13 @@ const setAnswers = (options, status, generateQuestion) => {
     });
   }
 };
+// Clears the question and the answers options from the DOM
+const clearQuestionAnswer = () => {
+  while (answersContainer.firstChild) {
+    answersContainer.removeChild(answersContainer.lastChild);
+  }
+  questionElement.innerHTML = "";
+};
 
 // Marks the current question
 const markQuestion = (answerGiven, correctAnswer) => {
@@ -71,10 +67,6 @@ const markQuestion = (answerGiven, correctAnswer) => {
 };
 // Loads the next question
 const nextQuestion = (status, generateQuestion) => {
-  while (answersContainer.firstChild) {
-    answersContainer.removeChild(answersContainer.lastChild);
-  }
-  questionElement.innerHTML = "";
   if (!status) {
     trivia(generateQuestion);
   }
@@ -95,9 +87,8 @@ const trivia = (generateQuestion) => {
     loadResults(numberOfQuestions, score);
     return 0; // exit
   }
-
+  clearQuestionAnswer();
   setQuestion(data.question);
-
   setAnswers(
     [...data.incorrect_answers, data.correct_answer],
     status,
